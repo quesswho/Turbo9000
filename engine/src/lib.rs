38955dc199ba729e6ec814e13b59fe83e50b74ed
Adrian Tudev <adrianotgonzaya07@gmail.com>
2026-08-24 23:32:22 +0200

    clean up engine API interface

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,14 +1,11 @@
-pub mod lookup;
 pub mod movegen;
 pub mod moves;
 pub mod perft;
 pub mod position;
 pub mod search;
 pub mod ttable;
-pub mod zobrist;
 
 pub const NAME: &str = "Turbo9000";
 
-pub fn hello() -> String {
-    format!("Hello World from {NAME}")
-}
+mod lookup;
+mod zobrist;
